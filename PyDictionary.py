c01e1d1fa168a2b71d0cf0032086c1e2336bb2c8
aditeyabaral <aditeya.baral@gmail.com>
2019-12-24 17:16:15 +0530

    Update PyDictionary.py
    Regular code cleanup :100:

--- PyDictionary.py
+++ PyDictionary.py
@@ -1,63 +1,46 @@
 from nltk.corpus import wordnet
 import enchant, nltk
-global d
-global flag
-flag = True
-global check
-d = enchant.Dict("en_GB")
-def accept():
-    check = False
-    global word
-    while check == False:
-        word = input("Enter word to search : ").strip()
-        check = d.check(word)
-        if check == False:
-            otherwords = d.suggest(word)
-            if len(otherwords) == 0:
-                print("Word not found in dictionary.")
-                choice = input("Would you like to add it to the dictionary? ")
-                if choice.upper() == 'YES':
-                    d.add_to_pwl(word)
-                    flag = False
-                    break
-            else:
-                print("Did you mean", otherwords[0]+'?')
-def results(word):
-    syn = wordnet.synsets(word)
-    dform  = {'n':'noun','v':'verb','a':'adjective','r':'adverb','s':'adjective satellite'}
-    ctr1 = 1
-    ctr2 = 97
-    for i in syn[:5]:
-        ctr2 = 97
-        definition, examples,form = i.definition(), i.examples(),i.pos()
-        print(str(ctr1)+'.',end = "")
-        print(dform[form], '-', word)
-        print("Definition :", definition.capitalize()+'.')
-        ctr1+=1
-        if len(examples)>0:
-            print("Usage : ")
-            for j in examples:
-                print (chr(ctr2)+'.',j.capitalize()+'.')
-                ctr2+=1
-        print()
-    antonyms = []
-    for i in syn:
-        for j in i.lemmas():
-            try:
-                antonyms.append(j.antonyms()[0].name())
-            except:
-                pass
-    if len(antonyms)>0:
-        print("Antonyms : ")
-        for i in antonyms:
-            print(i,end = ",")
-    print()
-while True:
-    accept()
-    if flag == True:
-        results(word)
+dictionary = enchant.Dict("en_GB")
+
+word = input("Enter word to search : ").strip()
+#check = dictionary.check(word)
+
+while not dictionary.check(word):
+    otherwords = dictionary.suggest(word)
+    if len(otherwords) == 0:
+        print("Word not found in dictionary.")
+        choice = input("Would you like to add it to the dictionary? ")
+        if choice.upper() == 'YES':
+            dictionary.add_to_pwl(word)
     else:
-        print("New word has been added to the dictionary.")
-    choice = input("\nSearch for another word? ")
-    if choice != '1':
-        break
+        print("Did you mean \"" +otherwords[0]+'\" ?')
+    word = input("Enter word to search : ").strip()
+
+syn = wordnet.synsets(word)
+dform  = {'n':'noun','v':'verb','a':'adjective','r':'adverb','s':'adjective satellite'}
+ctr1 = 1
+ctr2 = 97
+for i in syn:
+    ctr2 = 97
+    definition, examples,form = i.definition(), i.examples(),i.pos()
+    print(str(ctr1)+'.',end = "")
+    print(dform[form], '-', word)
+    print("Definition :", definition.capitalize()+'.')
+    ctr1+=1
+    if len(examples)>0:
+        print("Usage : ")
+        for j in examples:
+            print (chr(ctr2)+'.',j.capitalize()+'.')
+            ctr2+=1
+    print()
+antonyms = []
+for i in syn:
+    for j in i.lemmas():
+        try:
+            antonyms.append(j.antonyms()[0].name())
+        except:
+            pass
+if len(antonyms)>0:
+    print("Antonyms : ")
+    for i in antonyms:
+        print(i,end = ",")

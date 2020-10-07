6ac4d95b246a9158c11a35215dd9409523cef18b
Aditeya Baral <aditeya.baral@gmail.com>
2020-10-07 10:54:20 +0530

    update

--- app/main.py
+++ app/main.py
@@ -1,13 +1,57 @@
 import re
 import nltk
+from nltk.corpus import wordnet
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from flask_wtf.csrf import CSRFProtect
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-from utils import web_get_records, get_suggestions
+import enchant
 
+def get_suggestions(word):
+    return dictionary.suggest(word)
+
+
+def web_get_records(word):
+    if not dictionary.check(word):
+        return None
+    resp = ""
+    syn = wordnet.synsets(word)
+    dform = {
+        "n": "noun",
+        "v": "verb",
+        "a": "adjective",
+        "r": "adverb",
+        "s": "adjective satellite",
+    }
+    ctr1 = 1
+    ctr2 = 97
+    for i in syn[:10]:
+        ctr2 = 97
+        definition, examples, form = i.definition(), i.examples(), i.pos()
+        resp = resp + str(ctr1) + "." + "\n"
+        resp = resp + dform[form] + "-" + word + "\n"
+        resp = resp + "Definition : " + definition.capitalize() + "." + "\n"
+        ctr1 += 1
+        antonyms = []
+        if len(examples) > 0:
+            resp = resp + "Usage : " + "\n"
+            for j in examples:
+                resp = resp + chr(ctr2) + ". " + j.capitalize() + "." + "\n"
+                ctr2 += 1
+        for j in i.lemmas():
+            try:
+                antonyms.append(j.antonyms()[0].name())
+            except IndexError:
+                pass
+    if len(antonyms) > 0:
+        resp = resp + "Antonyms : " + "\n"
+        for i in antonyms:
+            resp = resp + i + "\n"
+    return resp
+
+dictionary = enchant.Dict("en_GB")
 nltk.data.path.append('../nltk_data/')
 
 try:
